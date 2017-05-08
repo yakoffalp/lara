9c4e074cd1fe0814c01e88460bc9455666c2c0ac
Yakov F <yakoffalp@gmail.com>
2017-05-08 14:56:59 +0300

    first commit

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return view('user.profile', ['user' => User::findOrFail($id)]);
+    }
+
+    public function listing()
+    {
+        return view('catalog.list', ['products' =>[0 => 'Bag', 1 => 'Coat']]);
+    }
+}
